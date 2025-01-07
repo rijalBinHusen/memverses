a020130807e26e42fe67e1c75fa5690c7abf7330
rijalBinHusen <jeryzal333@gmail.com>
2025-01-08 06:56:24 +0700

    import env from dynamic public

--- src/scipts/fetch.ts
+++ src/scipts/fetch.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_HOST_URL } from "$env/static/public";
+import { PUBLIC_HOST_URL } from "$env/dynamic/public";
 
 export async function fetchData(url: string) {
     // Try to fetch from cache first
