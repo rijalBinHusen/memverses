73891c9868392fb1b2f1f4654216cb6b7d840670
rijalbinhusen <jeryzal333@gmail.com>
2026-08-20 13:27:13 +0700

    function to read verse id

--- src/routes/verses/Chapter.svelte
+++ src/routes/verses/Chapter.svelte
@@ -84,22 +84,31 @@
 		retrieveChapterToRead();
 	}
 
-	function moveToFolder(e: any) {
-		
-        if (typeof window === "undefined") return;
-		const idFolderDestination = e.detail as string
+	function getVerseId(): number|undefined {
+		if (typeof window === "undefined") return;
         const params = new URLSearchParams(window.location.search);
 		const folderId = params.get('id-folder'); // folder id
 		const chaptersId = params.get('id'); // chapter id
         const verseNumber = params.get('verse'); // verse number
 		const findIndex = chapters.findIndex((rec) => rec.idFolder == folderId && rec.chapter == Number(chaptersId) && rec.verse == Number(verseNumber))
 
-		chapterOperation.moveVerseToFolder(
-			chapters[findIndex].id,
-			idFolderDestination,
-		);
+		return chapters[findIndex].id;
+	}
 
-		retrieveChapterToRead();
+	function moveToFolder(e: any) {
+		
+        if (typeof window === "undefined") return;
+		const idFolderDestination = e.detail as string
+		const verseId = getVerseId();
+
+		if(verseId) {
+			chapterOperation.moveVerseToFolder(
+				verseId,
+				idFolderDestination,
+			);
+			retrieveChapterToRead();
+		}
+
 		toggleModal();
 	}
 
@@ -150,13 +159,10 @@
         if (typeof window === "undefined") return;
 
 		const confirm = window.confirm("Apakah anda yakin untuk hapus ayat");
-		if(!confirm) return;
-        const params = new URLSearchParams(window.location.search);
-		const folderId = params.get('id-folder'); // folder id
-		const chaptersId = params.get('id'); // chapter id
-        const verseNumber = params.get('verse'); // verse number
-		const findIndex = chapters.findIndex((rec) => rec.idFolder == folderId && rec.chapter == Number(chaptersId) && rec.verse == Number(verseNumber))
-		chapterOperation.removeVerse(chapters[findIndex].id)
+		const verseId = getVerseId();
+		if(!confirm || !verseId) return;
+		
+		chapterOperation.removeVerse(verseId)
 		retrieveChapterToRead()
 	}
 
